309af052d6b84b3de2f59ec187b3891c8e619509
AlexCavalcantiJordao <alex37705732@gmail.com>
2024-11-17 12:55:12 -0300

    Instruções

---
+++ Instrução.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    lista = [2, 4, 5, 8, 10, 12, 14]
+    for n in lista:
+        if(n % 2 != 0):
+            print("Um número impar foi ignorado.")
+            continue
+        else:
+            print(n)
+
+        def calcula(x):
+            pass # Códificar posteriomente está em função....
